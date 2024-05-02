1692dd5027700ec86f2ac2973356f79bac79d770
[madhav sai] <[madhavsai.hvac@gmail.com]>
2024-05-02 20:41:34 +0530

    improved user  interaction

--- Network-Scanner/network-scanner.py
+++ Network-Scanner/network-scanner.py
@@ -1,21 +1,39 @@
-#! /usr/bin/env python 
+#!/usr/bin/env python
 
 import scapy.all as scapy
+import optparse
+from colorama import init, Fore
+
+# Initialize colorama
+init()
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="ip", help="[-] Enter the Subnet range of the IP or IP Address\n"
+                                                        "            [-] Usage: python3 network-scanner.py -t 192.168.0.0/24")
+    (options, arguments) = parser.parse_args()
+    if not options.ip:
+        parser.error("[-] Please specify a target. Use --help for more info.")
+    return options
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast,timeout=1,verbose=False)[0]
-    print("IP\t\t\t MAC Adress\n------------------------------------------------------------------------------------")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    
+    clients_list = []
     for element in answered_list:
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)
-    
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
 
+def print_result(results_list):
+    print(Fore.GREEN + "[+] IP\t\t\tMAC Address\n-----------------------------------------")
+    for client in results_list:
+        print("[+]" +  client["ip"] + '\t\t' + client["mac"])
 
-    
-scan("10.10.10.0/24")
-
-
+options = get_arguments()
+scan_result = scan(options.ip)
+print_result(scan_result)
 
